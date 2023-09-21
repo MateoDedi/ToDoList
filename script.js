a394f4e2434e86f491be4b0b3064b3deb6cce0d6
MateoDedi <mateod@live.com>
2023-09-21 09:53:49 +0200

    4

--- script.js
+++ script.js
@@ -3,27 +3,28 @@ let myArray = [];
 let spliceArray;
 
 while (command != "quit") {
-    command = prompt("Enter one of the following commands: \n   new: add a todo \n   list: display the todo \n   delete: remove a todo \n   quit: exit the todo");
+
+    command = prompt("Enter one of the following commands: \n   new: add a tdo \n   list: display the todo \n   delete: remove a todo \n   quit: exit the todo");
 
     if (command.trim() == "new") {
         let newTodo = prompt("Enter a new ToDo:");
         myArray.push(newTodo);
-        document.write(newTodo + "<br>");
+        console.log(newTodo);
     } else if (command.trim() == "list") {
-        document.write("<strong>********** TODO LIST **********</strong><br>");
+        console.log("********** TODO LIST **********");
         for (let i = 0; i < myArray.length; i++) {
-            document.write(`${i + 1}. ${myArray[i]}<br>`);
+            console.log(`${i+ 1}.  ${myArray[i]}`);
         }
     } else if (command.trim() == "delete") {
         let todoNumber = parseInt(prompt("Enter the number of the todo to delete:"));
 
         if (isNaN(todoNumber) || todoNumber < 1 || todoNumber > myArray.length) {
-            document.write("Invalid input. Please enter a valid number.<br>");
+        console.log("Invalid input. Please enter a valid number.");
         } else {
-            myArray.splice(todoNumber - 1, 1);
-            document.write(`Todo number ${todoNumber} deleted.<br>`);
+        myArray.splice(todoNumber - 1, 1);
+        console.log(`Todo number ${todoNumber} deleted.`);
         }
     } else {
-        alert("Enter a valid command: \n   new: add a todo \n   list: display the todo \n   delete: remove a todo \n   quit: exit the todo");
+        alert("Enter a valid command: \n   new: add a tdo \n   list: display the todo \n   delete: remove a todo \n   quit: exit the todo")
     }
 }
